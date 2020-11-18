66aaa34b1df907894066cd9f3b466c3ef158dec8
José Carlos López Arriaga <44308974+CarlosLopezNubes5@users.noreply.github.com>
2020-11-17 18:26:03 -0600

    Update Code

--- python-implementation/imhist.py
+++ python-implementation/imhist.py
@@ -1,29 +1,60 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.image as mpimg
+from rgb2gray import rgb2gray_avge
+import sys
+import os
 
-# Function to obtain the histogram of an image
-def imhist(Image):
-    """
-    Calculate the histogram of a grayscale image
+def imhist(img, *args, **kwargs):
+  """
+  Calculate the histogram of a grayscale image
 
-    Parameters
-    ----------------
-    Image : The function receives a grayscale image of N * M dimensions
+  Parameters
+  ----------------
+  img : The function receives a grayscale image of N * M dimensions
 
-    Returns
-    ----------------
-    ImageHistogram : The function returns the histogram of the image on a scale of 0 to 255
-    """
+  Returns
+  ----------------
+  H : The function returns the array with the image histogram values on a scale
+  from 0 to 255 and plots the histogram
+  """
 
-    I = np.uimt8(Image)
-    [r, c] = In.shape
-    H = np.zeros((256, 1))
+  if len(img.shape) > 2:
+    raise ValueError('The image is not in grayscale')
+  else:
+    tmp = np.uint8(img)
+    H = np.zeros((255, 1))
 
-    for i in range (r):
-        for j in range (c):
-            k = I[i, j]
-            H[k] = H[k] + 1
+    for i in range(img.shape[0]):
+      for j in range(img.shape[1]):
+        K = tmp[i, j]
+        H[K] += 1
 
-    ImageHistogram = H
-    plt.stem(ImageHistogram, linefmt = 'k-', markerfmt = 'none', basefmt = 'r-')
-    plt.title("Histogram")
+    fig, ax = plt.subplots(1, 1)
+    ax.set_title(*args, **kwargs)
+    ax.stem(H, linefmt = 'k-', markerfmt = 'none', basefmt = 'k-', use_line_collection=True)
+    ax.set_xlim(0, 255)
+    ax.grid('on')
+    ax.set_xticklabels([])
+
+    cmap = plt.get_cmap('gray', 255)
+    norm = mpl.colors.Normalize(vmin=0, vmax=255)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+
+    fig.colorbar(sm, orientation='horizontal')
+    fig.show()
+
+def main():
+    abspath = os.getcwd()
+    sys.path.append(abspath)
+    img_path = '/examples/Lenna.png'
+    img = mpimg.imread(abspath + img_path)
+
+    imhist(rgb2gray_avge(img), label='Histogram')
+
+if __name__ == '__main__':
+    main()
--- python-implementation/rgb2gray.py
+++ python-implementation/rgb2gray.py
@@ -1,86 +1,82 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 
 # Implementation of algorithms to convert an RGB image to grayscale (Average, Luminosity).
 
-def rgb2grayAverage(Image):
-    """
-    Conversion of a RGB image to a gray image by the Average method
+def rgb2gray_avge(img):
+  """
+  Conversion of a RGB image to a gray image by the Average method
 
-    Parameters
-    ----------------
-    Image : The function receives a grayscale image of N * M dimensions
+  Parameters
+  ----------------
+  img : The function receives a grayscale image of N * M dimensions
 
-    Returns
-    ----------------
-    IOUT : The function returns a grayscale image by the average method
-    """
+  Returns
+  ----------------
+  OUT : The function returns a grayscale image by the average method
+  """
 
-    Im = Image
-    [r, c, ch] = Im.shape
-    IOUT = np.zeros((r, c), dtype = np.uint8)
+  [r, c, ch] = img.shape
+  OUT = [([0] * c) for i in range(r)]
+  # OUT = np.zeros((r, c), dtype = np.uint8)
 
-    ImR = np.double(Im[:,:,0])
-    ImG = np.double(Im[:,:,1])
-    ImB = np.double(Im[:,:,2])
+  R = np.double(img[:, :, 0])
+  G = np.double(img[:, :, 1])
+  B = np.double(img[:, :, 2])
 
-    IOUT = np.uint8((ImR + ImG + ImB) / ch)
+  OUT = np.uint8((R + G + B) / ch)
 
-    return IOUT
+  return OUT
 
-def rgb2grayLuminosity(image):
+def rgb2gray_lmsty(img):
     """
     Conversion of a RGB image to a gray image by the Luminosity method
 
     Parameters
     ----------------
-    Image : The function receives a grayscale image of N * M dimensions
+    img : The function receives a grayscale image of N * M dimensions
 
     Returns
     ----------------
-    IOUT : The function returns a grayscale image by the Luminosity method
+    OUT : The function returns a grayscale image by the Luminosity method
     """
 
-    Im = image
-    [r, c, ch] = Im.shape
+    [r, c, ch] = img.shape
 
-    IOUT = np.zeros((r, c), dtype = np.uint8)
+    OUT = np.zeros((r, c), dtype = np.uint8)
 
-    ImR = np.double(Im[:,:,0])
-    ImG = np.double(Im[:,:,1])
-    ImB = np.double(Im[:,:,2])
+    R = 0.2125 * (np.double(img[:,:,0]))
+    G = 0.7154 * (np.double(img[:,:,1]))
+    B = 0.0721 * (np.double(img[:,:,2]))
 
-    R = 0.21 * ImR
-    G = 0.72 * ImG
-    B = 0.07 * ImB
+    OUT = np.uint8(R + G + B)
 
-    IOUT = np.uint8(R + G + B)
+    return OUT
 
-    return IOUT
-
-def rgb2grayLigthness(Image):
+def rgb2gray_lgtns(img):
     """
     Conversion of a RGB image to a gray image by the Ligthness method
 
     Parameters
     ----------------
-    Image : The function receives a grayscale image of N * M dimensions
+    img : The function receives a grayscale image of N * M dimensions
 
     Returns
     ----------------
-    IOUT : The function returns a grayscale image by the Ligthness method
+    OUT : The function returns a grayscale image by the Ligthness method
     """
 
-    Im = Image
-    [r, c, ch] = Im.shape
-    IOUT = np.zeros((r, c), dtype = np.uint8)
+    [r, c, ch] = img.shape
+    OUT = np.zeros((r, c), dtype = np.uint8)
 
-    R = np.double(Im[:,:,0])
-    G = np.double(Im[:,:,1])
-    B = np.double(Im[:,:,2])
+    R = np.double(img[:,:,0])
+    G = np.double(img[:,:,1])
+    B = np.double(img[:,:,2])
 
-    Mmax = np.asarray(np.maximum(np.maximum(R, G), B))
-    Mmin = np.asarray(np.minimum(np.minimum(R, G), B))
+    max_ = np.asarray(np.maximum(np.maximum(R, G), B))
+    min_ = np.asarray(np.minimum(np.minimum(R, G), B))
 
-    IOUT = np.uint8((Mmax + Mmin) / 2)
+    OUT = np.uint8((max_ + min_) / 2)
 
-    return IOUT
+    return OUT
