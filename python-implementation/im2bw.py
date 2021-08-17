39b21612a1fbec872f9a646ebe89cd0ff0b44917
José Carlos López Arriaga <carlos_gara_1507@hotmail.com>
2021-08-17 16:14:59 -0500

    Update Code im2bw.py

--- python-implementation/im2bw.py
+++ python-implementation/im2bw.py
@@ -1,4 +1,22 @@
-def im2bw(Image, umbral):
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.image as mpimg
+from rgb2gray import rgb2gray_avge
+
+def img2uint8(img):
+    '''
+    Convert image to 8-bit format [0, 255].
+    '''
+
+    vmin = img.min()
+    vmax = img.max()
+
+    img = ((img - vmin) / (vmax - vmin)) * 255.0
+
+    return np.uint8(img)
+
+def im2bw(img, threshold):
     """
     Convert an RGB image to a binary image (values ​​from 0 to 1)
 
@@ -12,20 +30,17 @@ def im2bw(Image, umbral):
     IOUT : The function returns a binary image
     """
 
-    # The function receives a grayscale image and a threshold value
-    I = np.double(Image)
-    U = umbral
-    [r, c] = I.shape
-    IOUT = np.zeros((r, c), dtype = np.double)
+    np.putmask(img, img <= threshold, 0)
+    np.putmask(img, img > threshold, 1)
 
-    for i in range(r):
-        for j in range(c):
-            k = I[i, j]
-            if (k <= U):
-                IOUT[i, j] = 0
-            else:
-                IOUT[i, j] = 1
+    return np.uint8(img)
 
-    IOUT = np.uint8(IOUT)
+if __name__ == '__main__':
+    img = mpimg.imread('./images/Lenna.png')
 
-    return IOUT
+    if img.dtype != np.uint8:
+        img = img2uint8(img)
+
+    imgbw = im2bw(rgb2gray_avge(img), 120)
+    plt.imshow(imgbw, cmap='gray')
+    plt.show()
